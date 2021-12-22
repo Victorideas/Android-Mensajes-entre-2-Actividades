31cc79556a4d5a741b96653c306899fc04413c8d
Victor <91167053+Victorideas@users.noreply.github.com>
2021-12-22 22:06:46 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.chat;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private EditText mMessageEditText;
+    public static final String EXTRA_MESSAGE = "hola"; //clave de la array
+
+    public static final int TEXT_REQUEST=1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText=findViewById(R.id.editTest_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    public void launchSecondActivity(View view) {
+       Log.d(LOG_TAG,"Clic en boton");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
---
+++ SecondActivity.java
@@ -0,0 +1,51 @@
+package com.example.chat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_Reply = "hola"; //clave de la array
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+        mReply = findViewById(R.id.editTest_second);
+
+    }
+    public void returnReply(View view){
+        String reply = mReply.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_Reply, reply);
+
+        setResult(RESULT_OK, replyIntent);
+        finish();
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply =
+                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+}
